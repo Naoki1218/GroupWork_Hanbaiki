c3fb715ec5b8f1c970c95f8a967b4dec7950e438
nakajima naoki <naoki9890@yahoo.co.jp>
2020-06-17 19:09:43 +0900

    6/17修正版

--- hanbaiki.rb
+++ hanbaiki.rb
@@ -1,40 +1,68 @@
 class Drink
-  def self.cola
-    self.new 120
+  attr_reader :name, :price
+
+  def initialize(name, price)
+    @name = name
+    @price = price
   end
 end
+
 class VendingMachine
-  # ステップ０　お金の投入と払い戻しの例コード
-  # ステップ１　扱えないお金の例コード
-  # 10円玉、50円玉、100円玉、500円玉、1000円札を１つずつ投入できる。
+
+  attr_reader :slot_money, :sale_amount, :drinks
+
   MONEY = [10, 50, 100, 500, 1000].freeze
-  # （自動販売機に投入された金額をインスタンス変数の @slot_money に代入する）
   def initialize
-    @sale_amount = 0
-    # 最初の自動販売機に入っている金額は0円
+    @drinks = [{ drink: Drink.new('cola', 120), count: 5 }]
     @slot_money = 0
-     @juice = {}
-      5.times{ Drink.cola }
+    @sale_amount = 0
   end
-  # 投入金額の総計を取得できる。
+
   def current_slot_money
-    # 自動販売機に入っているお金を表示する
     @slot_money
   end
-  # 10円玉、50円玉、100円玉、500円玉、1000円札を１つずつ投入できる。
-  # 投入は複数回できる。
+
+  def current_sale_amount
+    @sale_amount
+  end
+
   def slot_money(money)
-    # 想定外のもの（１円玉や５円玉。千円札以外のお札、そもそもお金じゃないもの（数字以外のもの）など）
-    # が投入された場合は、投入金額に加算せず、それをそのまま釣り銭としてユーザに出力する。
     return false unless MONEY.include?(money)
-    # 自動販売機にお金を入れる
     @slot_money += money
   end
-  # 払い戻し操作を行うと、投入金額の総計を釣り銭として出力する。
+
+  def select_drink(drink_name)
+      drinks.find do |drink|
+      drink[:drink].name == drink_name
+    end
+  end
+
+  def add_drink(name, price, count)
+    @drinks << {drink: Drink.new(name, price), count: count}
+  end
+
+  def purchase(drink_name)
+    drink = select_drink(drink_name)
+    if @slot_money >= drink[:drink].price
+       @slot_money -= drink[:drink].price
+       @sale_amount += drink[:drink].price
+      drink[:count] -= 1
+    end
+  end
+
+  def purchasable_drinks_list
+    purchasable_drinks.map do |drink|
+     drink[:drink].name
+    end
+  end
+
+  def purchasable_drinks
+    drinks.select do |drink|
+      drink[:drink].price <= @slot_money && drink[:count] > 0
+    end
+  end
   def return_money
-    # 返すお金の金額を表示する
     puts @slot_money
-    # 自動販売機に入っているお金を0円に戻す
     @slot_money = 0
   end
 end
