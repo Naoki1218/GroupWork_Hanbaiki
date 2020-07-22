b9f95b4d1f91f5cec3735f6f3da607dc8d25b4fa
nakajima naoki <naoki9890@yahoo.co.jp>
2020-07-22 13:27:30 +0900

    puts 追加

--- hanbaiki.rb
+++ hanbaiki.rb
@@ -19,16 +19,17 @@ class VendingMachine
   end
 
   def current_slot_money
-    @slot_money
+    puts "現在の投入金額は#{@slot_money}円です！"
   end
 
   def current_sale_amount
-    @sale_amount
+    puts "現在の売上高は#{@slot_amount}円です！"
   end
 
   def slot_money(money)
     return false unless MONEY.include?(money)
     @slot_money += money
+    puts "#{money}円が投入されました！"
   end
 
   def select_drink(drink_name)
@@ -39,19 +40,26 @@ class VendingMachine
 
   def add_drink(name, price, count)
     if @drinks.none? { |drink| drink[:drink].name == name}
-    @drinks << {drink: Drink.new(name, price), count: count}
-  else
-    drink = select_drink(name)
-    drink[:count] += count
+      @drinks << {drink: Drink.new(name, price), count: count}
+      puts "#{name}(#{price}円)が#{count}本追加されました！"
+    else
+      drink = select_drink(name)
+      drink[:count] += count
+      puts "#{name}が#{count}本追加されました！"
     end
   end
 
   def purchase(drink_name)
     drink = select_drink(drink_name)
-    if @slot_money >= drink[:drink].price
+    if @slot_money >= drink[:drink].price && drink[:count] > 0
        @slot_money -= drink[:drink].price
        @sale_amount += drink[:drink].price
       drink[:count] -= 1
+      puts "お買い上げいただきありがとうございます！#{drink_name}です！"
+    elsif @slot_money >= drink[:drink].price && drink[:count] = 0
+      puts "売り切れです！ごめんなさい😭"
+    elsif @slot_money < drink[:drink].price && drink[:count] > 0
+      puts "お金が足りません！！😭"
     end
   end
 
@@ -62,10 +70,11 @@ class VendingMachine
   end
 
   def purchasable_drinks
-    drinks.select do |drink|
+    @drinks.select do |drink|
       drink[:drink].price <= @slot_money && drink[:count] > 0
     end
   end
+  
   def return_money
     puts @slot_money
     @slot_money = 0
